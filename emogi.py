a075cee77cad427aa2dd20d46778cf90b42bbcf2
Chrys_David <chrysdavidbr12@gmail.com>
2024-02-18 23:01:08 +0000

    first commit

---
+++ emogi.py
@@ -0,0 +1,8 @@
+list_Emogi = ["\U0001F917", "\U0001F637", "\U0001F62A", "\U0001F618", "\U0001F600", "\U0001F604", "\U0001F60D", "\U0001F929", "\U0001F618"]
+
+print(list_Emogi)
+print("\n")
+
+for i in range(0, len(list_Emogi) - 1):
+    print(list_Emogi[i])
+
